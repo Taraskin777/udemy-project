35bc759ca685961ed5ae2625c677ceebf0222926
Taras <taraskin2012@gmail.com>
2022-03-12 15:45:39 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам є 15?", "");
+// console.log(typeof (answer));
+
+const answers = [];
+
+answers[0] = prompt("Яке ваше ім'я ? ", "");
+answers[1] = prompt("Яке ваше прізвище ? ", "");
+answers[2] = prompt("Скільки Вам років ? ", "");
+
+console.log(answers);
