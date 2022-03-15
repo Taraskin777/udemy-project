a1417288880ac9d8f8f849b84f8970d4cf61198b
Taras Noga <taraskin2012@gmail.com>
2022-03-15 18:16:02 +0200

    add personalMovieDB

--- js/script.js
+++ js/script.js
@@ -18,4 +18,22 @@
 // 'Сколько фильмов вы уже посмотрели?'
 
 let numberOfFilms = prompt("Скільки фільмів Ви вже подивилися?", "");
+console.log(numberOfFilms);
 alert(`Ви подивилися вже ${numberOfFilms} фільмів!`);
+
+// 2) Создать объект personalMovieDB и в него поместить такие свойства:
+//     - count - сюда передается ответ на первый вопрос
+//     - movies - в это свойство поместить пустой объект
+//     - actors - тоже поместить пустой объект
+//     - genres - сюда поместить пустой массив
+//     - privat - в это свойство поместить boolean(логическое) значение false
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
