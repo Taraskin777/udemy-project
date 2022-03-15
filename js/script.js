a0aedbab9fb6debd82aa68b92f206ec55e03e031
Taras Noga <taraskin2012@gmail.com>
2022-03-15 18:08:02 +0200

    add number of films

--- js/script.js
+++ js/script.js
@@ -6,10 +6,15 @@
 // const answer = prompt("Вам є 15?", "");
 // console.log(typeof (answer));
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt("Яке ваше ім'я ? ", "");
-answers[1] = prompt("Яке ваше прізвище ? ", "");
-answers[2] = prompt("Скільки Вам років ? ", "");
+// answers[0] = prompt("Яке ваше ім'я ? ", "");
+// answers[1] = prompt("Яке ваше прізвище ? ", "");
+// answers[2] = prompt("Скільки Вам років ? ", "");
 
-console.log(answers);
+// console.log(answers);
+
+// 1
+
+let numberOfFilms = prompt("Скільки фільмів Ви вже подивилися?", "");
+alert(`Ви подивилися вже ${numberOfFilms} фільмів!`);
